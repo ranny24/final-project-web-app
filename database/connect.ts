ad16d26eda4f2da688dcec6b1981f90acbf13966
Mona Lisa Octocat <raniamet.90@gmail.com>
2023-07-13 14:17:10 +0200

    Please enter the commit message for your changes. Lines starting

--- database/connect.ts
+++ database/connect.ts
@@ -1,24 +1,20 @@
 import 'server-only';
-import { config } from 'dotenv-safe';
 import { headers } from 'next/headers';
 import postgres, { Sql } from 'postgres';
+import { setEnvironmentVariables } from '../util/config.mjs';
 
-/* if (!process.env.FLY_IO) config();
+setEnvironmentVariables();
 
 declare module globalThis {
   let postgresSqlClient: Sql;
-} */
+}
 
 // Connect only once to the database
 // https://github.com/vercel/next.js/issues/7811#issuecomment-715259370
 function connectOneTimeToDatabase() {
   if (!('postgresSqlClient' in globalThis)) {
     globalThis.postgresSqlClient = postgres({
-      host: process.env.POSTGRES_HOST || process.env.PG_HOST,
-      username: process.env.POSTGRES_USER || process.env.PGUSERNAME,
-      password: process.env.POSTGRES_PASSWORD || process.env.PGPASSWORD,
-      database: process.env.POSTGRES_DATABASE || process.env.PGDATABASE,
-      ssl: !!process.env.POSTGRES_URL,
+      ssl: Boolean(process.env.POSTGRES_URL),
       transform: {
         ...postgres.camel,
         undefined: null,
